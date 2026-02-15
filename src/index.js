5a0e7f6b04b564d81d45680e0de3bf0a84e043cf
Sean <sean@seriousbusiness.international>
2026-02-14 22:27:02 -0800

    update health routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import helmet from "helmet";
 import pino from "pino-http";
-
+import healthRouter from "./routes/health.js";
 import threadsRouter from "./routes/threads.js";
 
 const app = express();
 app.use(helmet());
 app.use(pino());
 app.use(express.json());
+
+app.use(healthRouter);
 
 app.get("/healthz", (req, res) => res.json({ ok: true }));
 
